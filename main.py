288f18f7dac7c070736f9091f7af7b3a6529867c
chriskayomb <ibond.chris@gmail.com>
2023-11-06 20:58:06 +0100

    read from the file and categorize the frames

---
+++ main.py
@@ -0,0 +1,62 @@
+# Participant:
+# Angela Makhanu
+# Carmel Kevin
+# Stephany
+# Chris Kayomb
+
+class Painting:
+    def __init__(self, paint_id, paint_type, paint_tag_num, paint_tag_lst):
+        self._paint_id = paint_id
+        self._paint_type = paint_type
+        self._paint_tag_num = paint_tag_num
+        self._paint_tag_lst = paint_tag_lst
+
+    def __str__(self):
+        return f'{self._paint_id} {self._paint_type} {self._paint_tag_num} {self._paint_tag_lst}'
+
+
+def read_data(file):
+    try:
+        with open(file, 'r') as my_file:
+            lst = my_file.readlines()
+            return lst
+    except IOError as e:
+        print(f"An error occurred: {e}")
+
+
+def categorize_frames(data):
+    frame_id = 0
+    frame_landscape = []
+    frame_portrait = []
+
+    for line in data[1:]:
+        line_parts = line.split()
+        frame_type = line_parts[0]
+        frame_tag_num = int(line_parts[1])
+        frame_tag_lst = set(line_parts[2:])
+        painting = Painting(frame_id, frame_type, frame_tag_num, frame_tag_lst)
+
+        if frame_type == 'L':
+            frame_landscape.append(painting)
+        else:
+            frame_portrait.append(painting)
+        frame_id += 1
+
+    print("Landscape Frames:", frame_landscape)
+    print("Portrait Frames:", frame_portrait)
+
+    return frame_landscape, frame_portrait
+
+
+if __name__ == "__main__":
+    data = read_data('Data/0_example.txt')
+    landscape, portrait = categorize_frames(data)
+    
+    print("Landscape Frames:")
+    for painting in landscape:
+        print(painting)
+    
+    print("Portrait Frames:")
+    for painting in portrait:
+        print(painting)
+
