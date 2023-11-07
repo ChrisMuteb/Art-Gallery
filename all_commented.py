f5ffd695b4834ccc99fb3f0a903ed9ac3fc527e7
chriskayomb <ibond.chris@gmail.com>
2023-11-07 10:59:07 +0100

    combination of two portrait frames into a frameglass

---
+++ all_commented.py
@@ -0,0 +1,35 @@
+# test local_score_function
+    # frameAs = portrait_lst[0]._paint_tag_lst
+    # frameBs = landscape_lst[0]._paint_tag_lst
+    # print(frameAs)
+    # print(frameBs)
+    # print(local_robotic_satisfaction(frameAs, frameBs))
+
+    # test sort painting based on their tag number
+    # sorted_landscape = sort_painting(landscape_lst)
+    # print(sorted_landscape[len(sorted_landscape)-1]._paint_tag_num)
+    # for painting in sorted_landscape:
+    #     # print(painting)
+        
+    #     print(painting)
+
+    # combined portrait
+    # lst_frameglasses = []
+    # result_combine_portrait = combine_portrait_frameglass(portrait_lst[0]._paint_tag_lst, portrait_lst[1]._paint_tag_lst)
+    
+    # print(result_combine_portrait)
+    # lst_frameglasses.append(result_combine_portrait)
+
+    # result_landscape_A = landscape_lst[0]._paint_tag_lst
+    # print(result_landscape_A)
+    # lst_frameglasses.append(result_landscape_A)
+
+    # result_landscape_B = landscape_lst[1]._paint_tag_lst
+    # print(result_landscape_B)
+    # lst_frameglasses.append(result_landscape_B)
+
+    # print(lst_frameglasses) # display all the sets containing the 3 frameglasses.
+
+    # permutation_result = permutate_sets(lst_frameglasses)
+    # display_permutations(permutation_result)
+
--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # Stephany Lynn -- Member
 # Chris Kayomb -- Member
 
+from itertools import product
+
 class Painting:
     def __init__(self, paint_id, paint_type, paint_tag_num, paint_tag_lst):
         self._paint_id = paint_id
@@ -63,35 +65,67 @@ def local_robotic_satisfaction(frameglassA, frameglassB):
 def sort_painting(paintings):
     return sorted(paintings, key=lambda painting: painting._paint_tag_num)
 
+# combine 2 portrait frame. it takes 2 sets of _paint_tag_lst
+def combine_portrait_frameglass(frameglassA, frameglassB):
+    return frameglassA.union(frameglassB)
+
+# group portrait painting in pairs
+def group_paintings_in_pairs(paintings):
+    pairs = []
+
+    left_pointer = 0
+    right_pointer = len(paintings) - 1
+
+    # Iterate until the pointers meet or cross each other.
+    while left_pointer < right_pointer:
+        pair = (paintings[left_pointer], paintings[right_pointer])
+        pairs.append(pair)
+
+        # Move the pointers towards each other.
+        left_pointer += 1
+        right_pointer -= 1
+
+    # If there's one painting left in the middle (odd number of paintings), add it as a single element pair.
+    if left_pointer == right_pointer:
+        pairs.append((paintings[left_pointer],))
+
+    return pairs
+
+# permutate the sets and return a dict
+def permutate_sets(lst):
+    permutations = {}
+    
+    for i, permuted_set in enumerate(product(*lst)):
+        permutations[f'Permutation {i + 1}'] = set(permuted_set)
+    
+    return permutations
+
+def display_permutations(permutations):
+    for key, value in permutations.items():
+        print(f'{key}: {value}')
+
 if __name__ == "__main__":
-    data = read_data('Data/1_binary_landscapes.txt')
+    data = read_data('Data/110_oily_portraits.txt')
     landscape, portrait = categorize_frames(data)
     landscape_lst = []
     portrait_lst = []
 
-    print("Landscape Frames:")
+    # print("Landscape Frames:")
     for painting in landscape:
-        print(painting)
+        # print(painting)
         landscape_lst.append(painting)
     
-    print("Portrait Frames:")
+    # print("Portrait Frames:")
     for painting in portrait:
         # print(painting)
         portrait_lst.append(painting)
 
-    # test local_score_function
-    # frameAs = portrait_lst[0]._paint_tag_lst
-    # frameBs = landscape_lst[0]._paint_tag_lst
-    # print(frameAs)
-    # print(frameBs)
-    # print(local_robotic_satisfaction(frameAs, frameBs))
 
-    # test sort painting based on their tag number
-    sorted_landscape = sort_painting(landscape_lst)
-    print(sorted_landscape[len(sorted_landscape)-1]._paint_tag_num)
-    for painting in sorted_landscape:
-        # print(painting)
-        
-        print(painting)
+    paired_paintings = group_paintings_in_pairs(portrait_lst)
+    print("Pair Portrait Frames:")
+    for pair in paired_paintings:
+        for painting in pair:
+            print(painting)
 
 
+    
